29646eb1688b90d322133e83b178bb2c429e18a2
Pawan16123 <poojary.pawan16@gmail.com>
2020-01-17 18:41:27 +0530

    flexbox trail added and commented

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,6 @@
 var banner = document.getElementsByClassName("banner");
 var newsLetter = document.getElementsByClassName("newsletter");
+
 // var body = document.getElementById("body");
 console.log(newsLetter[0]);
 
@@ -7,7 +8,22 @@ newsLetter[0].style.marginTop = banner[0].offsetHeight-47+"px" ;
 window.addEventListener("resize",triggerMe);
 function triggerMe(){
     newsLetter[0].style.marginTop = banner[0].offsetHeight-47+"px" ;
-    console.log(width)
+    // console.log(width)
 }
 
+// Just for flex-box
+// var boxes = document.getElementsByClassName("boxes");
+
+
+// function getHeight() {
+//     height = Math.floor((Math.random() * 300) + 200);
+//     return height;
+// }
+
+// for(i=0; i<boxes.length; i++){
+//     console.log(boxes[i]);
+//     boxes[i].style.height = getHeight()+"px";
+//     console.log(height);
+
+// }
 
